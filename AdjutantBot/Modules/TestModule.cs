dca15bc869cd86ccaa916abbcba184af9fed7bb2
Ian Barrow <43844902+Banananaman91@users.noreply.github.com>
2019-04-23 14:26:43 +0100

    Update TestModule.cs
    Added mute and unmute commands, with mute role creation and permissions overwrite

--- AdjutantBot/Modules/TestModule.cs
+++ AdjutantBot/Modules/TestModule.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace AdjutantBot.Modules
 {
     public class TestModule : ModuleBase<SocketCommandContext>
     {
+        public const string MuteRoleName
+    = "Cone-of-Shame";
         [Command("ping"), Summary("pong!")]
         public async Task Ping()
         {
@@ -28,5 +31,70 @@ namespace AdjutantBot.Modules
             await targetUser.SendMessageAsync("You have been banned from the Game Development Society " + reason);
             await targetUser.BanAsync();
         }
+
+        [Command("mute"), Summary("Mute the specified user")]
+        public async Task MuteUser(IGuildUser targetUser)
+        {
+            var guild = targetUser.Guild;
+            var muteRole = await GetOrCreateMuteRoleAsync(guild);
+
+            if (targetUser.RoleIds.Contains(muteRole.Id))
+                throw new InvalidOperationException($"Discord user {targetUser} is already muted");
+
+            foreach (var channel in await guild.GetChannelsAsync())
+                await ConfigureChannelMuteRolePermissionsAsync(channel, muteRole);
+
+            await targetUser.AddRoleAsync(muteRole);
+            await ReplyAsync(targetUser + " has been muted");
+        }
+
+        [Command("unmute"), Summary("Mute the specified user")]
+        public async Task UnMuteUser(IGuildUser targetUser)
+        {
+            var guild = targetUser.Guild;
+            var unMuteRole = await GetOrCreateMuteRoleAsync(guild);
+
+            if (!targetUser.RoleIds.Contains(unMuteRole.Id))
+                throw new InvalidOperationException($"Discord user {targetUser} is not currently muted");
+
+            await targetUser.RemoveRoleAsync(unMuteRole);
+            await ReplyAsync(targetUser + " has been unmuted");
+        }
+
+        private async Task ConfigureChannelMuteRolePermissionsAsync(IGuildChannel channel, IRole muteRole)
+        {
+            var permissionOverwrite = channel.GetPermissionOverwrite(muteRole);
+            if (permissionOverwrite != null)
+            {
+                if ((permissionOverwrite.Value.AllowValue == _mutePermissions.AllowValue) &&
+                    (permissionOverwrite.Value.DenyValue == _mutePermissions.DenyValue))
+                    return;
+
+                await channel.RemovePermissionOverwriteAsync(muteRole);
+            }
+
+            await channel.AddPermissionOverwriteAsync(muteRole, _mutePermissions);
+        }
+
+        private async Task<IRole> GetOrCreateMuteRoleAsync(IGuild guild)
+            => guild.Roles.FirstOrDefault(x => x.Name == MuteRoleName)
+                ?? await guild.CreateRoleAsync(MuteRoleName);
+
+
+        private static readonly OverwritePermissions _mutePermissions
+            = new OverwritePermissions(
+                sendMessages: PermValue.Deny,
+                sendTTSMessages: PermValue.Deny,
+                createInstantInvite: PermValue.Deny,
+                embedLinks: PermValue.Deny,
+                attachFiles: PermValue.Deny,
+                mentionEveryone: PermValue.Deny,
+                useExternalEmojis: PermValue.Deny,
+                addReactions: PermValue.Deny,
+                useVoiceActivation: PermValue.Deny,
+                muteMembers: PermValue.Deny,
+                deafenMembers: PermValue.Deny,
+                moveMembers: PermValue.Deny,
+                speak: PermValue.Deny);
     }
 }
