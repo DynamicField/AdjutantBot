dbf9a15f11b87dc12192779ae018f71183c2c561
Mathias Storm <mathias.storm@outlook.de>
2019-05-16 16:54:31 +0200

    Remove Asp.net Core boilerplate

--- backend/AdjutantApi/Startup.cs
+++ backend/AdjutantApi/Startup.cs
@@ -18,7 +18,6 @@ namespace AdjutantApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -32,7 +31,6 @@ namespace AdjutantApi
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +39,6 @@ namespace AdjutantApi
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseMvc();
