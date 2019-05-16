54e32df4a930e5f4e55860983d14ac926ef210eb
Mathias Storm <mathias.storm@outlook.de>
2019-05-16 16:32:24 +0200

    Move Backend files into a backend folder

---
+++ backend/AdjutantApi/ViewModels/DisplayNameUpdate.cs
@@ -0,0 +1,8 @@
+namespace AdjutantApi.ViewModels
+{
+    public class DisplayNameUpdate
+    {
+        public string KeyValue { get; set; }
+        public string NewDisplayName { get; set; }
+    }
+}
---
+++ backend/AdjutantBotConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using AdjutantBot;
+
+namespace AdjutantBotConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                tcs.SetResult(true);
+            };
+
+            var t = new AdjutantBotClient().LaunchBotAsync();
+
+            await Task.WhenAll(t, tcs.Task);
+        }
+    }
+}
