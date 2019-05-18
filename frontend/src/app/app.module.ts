845f1e01f135d926339e7d864a68388790001d86
Mathias Storm <mathias.storm@outlook.de>
2019-05-18 12:14:28 +0200

    Frontend Gruntwork for styling and application structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginwithdiscordComponent } from './components/authorization/loginwithdiscord/loginwithdiscord.component';
+import { LoginComponent } from './views/authorization/login/login.component';
+import { PagenotfoundComponent } from './views/misc/pagenotfound/pagenotfound.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginwithdiscordComponent,
+    LoginComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/authorization/loginwithdiscord/loginwithdiscord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginwithdiscord',
+  templateUrl: './loginwithdiscord.component.html',
+  styleUrls: ['./loginwithdiscord.component.css']
+})
+export class LoginwithdiscordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
