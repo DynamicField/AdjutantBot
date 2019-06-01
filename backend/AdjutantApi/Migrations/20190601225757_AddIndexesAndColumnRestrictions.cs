abd467ee03d44c066814c8aa818e4c3e48e0f9e0
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-06-02 00:58:34 +0200

    Added a missing migration

---
+++ backend/AdjutantApi/Migrations/20190601225757_AddIndexesAndColumnRestrictions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdjutantApi.Migrations
+{
+    public partial class AddIndexesAndColumnRestrictions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "KeyValue",
+                table: "VerificationKeys",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscordUsername",
+                table: "AspNetUsers",
+                maxLength: 37,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VerificationKeys_KeyValue",
+                table: "VerificationKeys",
+                column: "KeyValue",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VerificationKeys_KeyValue",
+                table: "VerificationKeys");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "KeyValue",
+                table: "VerificationKeys",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscordUsername",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 37,
+                oldNullable: true);
+        }
+    }
+}
